9ffc27e23ce104a82966ad09c8ee70c0ec805be5
Johan <joletuar.dev@gmail.com>
2025-07-31 06:20:31 -0500

    chore: init project

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import { GlobalHandlerErrorFilter } from './modules/shared/infraestructure/http/nest/filters/global-handler-error.filter';
+import { InfraestructureHandlerErrorFilter } from './modules/shared/infraestructure/http/nest/filters/infraestructure-handler-error.filter';
+import { FormatApiResponse } from './modules/shared/infraestructure/http/nest/interceptors/format-api-response.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+    }),
+  );
+
+  app.useGlobalFilters(
+    new InfraestructureHandlerErrorFilter(),
+    new GlobalHandlerErrorFilter(),
+  );
+
+  app.useGlobalInterceptors(new FormatApiResponse());
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+void bootstrap();

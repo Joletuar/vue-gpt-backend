7cc1fb03818a28b28f24b26efd598ceb1cb90c9e
Johan <joletuar.dev@gmail.com>
2025-08-03 07:50:03 -0500

    feat: improve validation filter

--- src/modules/shared/infraestructure/http/nest/filters/validation-handler-error.filter.ts
+++ src/modules/shared/infraestructure/http/nest/filters/validation-handler-error.filter.ts
@@ -18,13 +18,21 @@ export class ValidationExceptionFilter implements ExceptionFilter {
 
     console.error(exception);
 
-    const exceptionResponse = exception.getResponse() as { message: string[] };
+    const exceptionResponse = exception.getResponse() as
+      | {
+          message: string[];
+        }
+      | { message: string };
+
+    const details = Array.isArray(exceptionResponse.message)
+      ? exceptionResponse.message.map((i) => i)
+      : exceptionResponse.message;
 
     res.status(HttpStatus.UNPROCESSABLE_ENTITY).json({
       statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
       error: {
         message: 'Parece que la solicitud no tiene el formato adecuado.',
-        details: exceptionResponse.message.map((i) => i),
+        details,
       },
       meta: {
         requestId: req.requestId || 'NO_ID',
